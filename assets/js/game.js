a114ddbc17d74547fcd031e3e6d017e45e677125
Plain Jane <jane99hsu@gmail.com>
2020-08-21 14:12:18 -0700

    added high score feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -157,22 +157,33 @@ var startGame = function() {
 // function to end the entire game
 var endGame = function() {
     // if player is still alive, player wins!
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+
+    window.alert("The game has now ended. Let's see how you did!");
+
+    // check localStorage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+    // if player has more money than the high score, player has new high score!
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
     } 
     else {
-        window.alert("You've lost your robot in battle.");
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
     }
 
     // ask player if they'd like to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
     if (playAgainConfirm) {
-        // restart the game
         startGame();
     } 
     else {
-        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+        window.alert("Thank you for playing Battlebots! Come back soon!");
     }
 }
 
@@ -231,11 +242,11 @@ var getPlayerName = function() {
 
 var playerInfo = {
     name: getPlayerName(),
-    health: 100,
+    health: 50,
     attack: 10,
     money: 10,
     reset: function() {
-        this.health = 100;
+        this.health = 50;
         this.money = 10;
         this.attack = 10;
     }, 
